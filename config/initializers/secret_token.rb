5bd1f3d8189671be0d694b1be0e758024741f10e
Lorenzo García <logaboyepub@gmail.com>
2013-02-05 18:32:27 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MiPrimeraApp::Application.config.secret_token = '3e548ab740af73b0c63f923f18b534ccd5355c2997c7a76647ef2119d28288b7cbdc75b74e7ead6bd71eeb18431be48a9e253ca9dbbc6bedab864fc91b8bd06b'
